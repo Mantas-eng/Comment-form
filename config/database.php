2d6b93dd7bb7ce8523ae82ebb3e1c52889aa12c0
Mantas-eng <68243914+Mantas-eng@users.noreply.github.com>
2025-01-03 18:35:09 +0200

    updated file structure

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 function get_db_connection() {
-    $db_url = parse_url(getenv('JAWSDB_URL'));
+    $db_url = parse_url(getenv('mysql://d1h5t0yoywxh7daf:mocg4v8i04t8vh2y@gp96xszpzlqupw4k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ui2butiwnvviyfgw'));
 
     $host = $db_url['host'];
     $user = $db_url['user'];
